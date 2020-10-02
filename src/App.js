eccf194f643ef21348cdeee2da25bf447560ab1d
Gaganmeet Bahri <gaganmeet.bahri96@gmail.com>
2020-10-02 09:31:32 +0530

    broken homepage to smaller components, finished styling on menu-items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import HomePage from './homepage.component'
+import HomePage from './pages/homepage/homepage.component'
 
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World
       <HomePage/>
     </div>
   );
