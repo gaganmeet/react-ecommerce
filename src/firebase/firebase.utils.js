87fae8df57c7c7aff7a4a528d568669e4f6adea1
Gaganmeet Bahri <gaganmeet.bahri96@gmail.com>
2020-11-21 11:03:56 +0530

    added firebase utils and added sign up authetication

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
     appId: "1:892012638558:web:7b3dc635ac630de3ba5fcf",
     measurementId: "G-19ZJZ2227G"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData 
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
+
+}
   
 firebase.initializeApp(config);
 
